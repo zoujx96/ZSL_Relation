afb775dc7b5f199278ccf786589a3ea12a0d7414
Jiaxu Zou <jiaxuz@gpu047.pvt.bridges.psc.edu>
2019-04-28 11:19:37 -0400

    model A and B completed

--- CUB_RN_base.py
+++ CUB_RN_base.py
@@ -31,12 +31,14 @@ TEST_EPISODE = args.test_episode
 LEARNING_RATE = args.learning_rate
 GPU = args.gpu
 
+np.set_printoptions(threshold=np.inf)
+
 class AttributeNetwork(nn.Module):
     """docstring for RelationNetwork"""
-    def __init__(self,input_size,hidden_size,output_size):
+    def __init__(self, input_size, hidden_size, output_size):
         super(AttributeNetwork, self).__init__()
-        self.fc1 = nn.Linear(input_size,hidden_size)
-        self.fc2 = nn.Linear(hidden_size,output_size)
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self,x):
         x = F.relu(self.fc1(x))
@@ -51,73 +53,102 @@ class RelationNetwork(nn.Module):
         self.fc2 = nn.Linear(hidden_size,1)
 
     def forward(self,x):
-
         x = F.relu(self.fc1(x))
         x = F.sigmoid(self.fc2(x))
         return x
     
 class IntegratedDataset(torch.utils.data.Dataset):
-    def __init__(self, features, labels, att_values, cer_values):
+    def __init__(self, features, labels, att_values):
         super(IntegratedDataset, self).__init__()
         self.features = features
         self.labels = labels
         self.att_values = att_values
-        self.cer_values = cer_values
 
     def __getitem__(self, index):
         single_feature, single_label = self.features[index], self.labels[index]
-        single_att_value, single_cer_value = self.att_values[index], self.cer_values[index]
+        single_att_value = self.att_values[index]
         
-        single_cer_value = single_cer_value / 4
-        return single_feature, single_label, single_att_value, single_cer_value
+        return single_feature, single_label, single_att_value
 
     def __len__(self):
         return len(self.features)
 
-def evaluate_attribute_network(model, criteria_1, criteria_2, eval_loader):
+def evaluate_attribute_network(model, criteria, eval_loader):
     model.eval()
     total_loss_att = 0.
-    total_loss_cer = 0.
     total_correct_num_att = 0.
-    total_correct_num_cer = 0.
     sample_num = 0
     with torch.no_grad():
-        for i, (batch_features, batch_labels, batch_att, batch_cer) in enumerate(eval_loader):
-            batch_features, batch_att, batch_cer = batch_features.float().cuda(), batch_att.float().cuda(), batch_cer.float().cuda()
+        for i, (batch_features, batch_labels, batch_att) in enumerate(eval_loader):
+            batch_features, batch_att = batch_features.float().cuda(), batch_att.float().cuda()
             pred_embeddings = model(batch_features)
 
             cat_batch_att = batch_att.long().view(-1)
-            cat_pred_att = torch.cat((pred_embeddings[:,0:312].unsqueeze(2), 1 - pred_embeddings[:,0:312].unsqueeze(2)), 2).view(-1, 2)
-                
-            #weights_att = torch.where(batch_att == torch.Tensor([1]).cuda(), torch.Tensor([10]).cuda(), torch.Tensor([1]).cuda())
-            #bce_att = nn.BCELoss(weight=weights_att).cuda()
-            #loss_att = bce_att(pred_embeddings[:,0:312], batch_att)
-            loss_att = criteria_1(torch.log(cat_pred_att + 1e-10), cat_batch_att)
-            #loss_att = criteria(pred_embeddings[:,0:312], batch_att)
-            batch_correct_att = ((pred_embeddings[:,0:312] < 0.5).int() == batch_att.int()).float().sum()
+            cat_pred_att = torch.cat((pred_embeddings.unsqueeze(2), 1 - pred_embeddings.unsqueeze(2)), 2).view(-1, 2)
 
-            # weights_cer = torch.where(batch_cer == torch.Tensor([1]).cuda(), torch.Tensor([10]).cuda(), torch.Tensor([1]).cuda())
-            # bce_cer = nn.BCELoss(weight=weights_cer).cuda()
-            loss_cer = criteria_2(pred_embeddings[:,312:624], batch_cer)
-            batch_correct_cer = ((pred_embeddings[:,312:624] * 4 + 0.5).int() == (batch_cer * 4).int()).float().sum()
+            loss_att = criteria(torch.log(cat_pred_att + 1e-10), cat_batch_att)
 
+            batch_correct_att = ((pred_embeddings < 0.5).int() == batch_att.int()).float().sum()
+            '''
             if i == 5:
-                print(pred_embeddings[14,0:312])
+                print(pred_embeddings[14])
                 print(batch_att[14])
-                print(pred_embeddings[14,312:624])
-                print(batch_cer[14])
-
+            '''
             total_loss_att += loss_att.item() * 312 * len(batch_att)
-            total_loss_cer += loss_cer.item() * 312 * len(batch_cer)
             total_correct_num_att += batch_correct_att
-            total_correct_num_cer += batch_correct_cer
             sample_num += len(batch_att)
 
     loss_att_mean = total_loss_att / (sample_num * 312)
-    loss_cer_mean = total_loss_cer / (sample_num * 312)
     acc_att = total_correct_num_att / (sample_num * 312)
-    acc_cer = total_correct_num_cer / (sample_num * 312)
-    return loss_att_mean, loss_cer_mean, acc_att, acc_cer
+    return loss_att_mean, acc_att
+
+def evaluate_relation_network(model, criteria, eval_loader, all_attributes):
+    model.eval()
+    total_loss_rel = 0.
+    total_correct_num_rel = 0
+    sample_num = 0
+    total_class_num = 0
+    with torch.no_grad():
+        for i, (batch_features, batch_labels, batch_att) in enumerate(eval_loader):
+            batch_att = batch_att.float().cuda()
+
+            sample_labels = set(batch_labels.squeeze().numpy().tolist())
+                    
+            sample_embeddings = torch.Tensor([all_attributes[i] for i in sample_labels]).cuda()
+
+            class_num = sample_embeddings.shape[0]
+
+            embeddings_bunch = sample_embeddings.unsqueeze(0).repeat(len(batch_att), 1, 1)
+            attributes_bunch = batch_att.unsqueeze(0).repeat(class_num, 1, 1)
+            attributes_bunch = torch.transpose(attributes_bunch, 0, 1)
+            
+            relation_pairs = torch.cat((embeddings_bunch, attributes_bunch), 2).view(-1, 624)
+            relations = model(relation_pairs).view(-1, class_num)
+
+            # re-build batch_labels according to sample_labels
+            sample_labels = np.array(list(sample_labels))
+            re_batch_labels = []
+            for label in batch_labels.numpy():
+                index = np.argwhere(sample_labels == label)
+                re_batch_labels.append(index[0][0])
+            re_batch_labels = torch.LongTensor(re_batch_labels)
+            one_hot_labels = torch.zeros(len(batch_att), class_num).scatter_(1, re_batch_labels.view(-1, 1), 1).cuda()
+            
+            loss_rel = criteria(relations, one_hot_labels)
+            batch_correct_rel = (torch.argmax(relations, 1) == re_batch_labels.cuda()).sum()
+            '''
+            if i == 5:
+                print(pred_embeddings[14])
+                print(batch_att[14])
+            '''
+            total_loss_rel += loss_rel.item() * class_num * len(batch_att)
+            total_correct_num_rel += batch_correct_rel.item()
+            sample_num += len(batch_att)
+            total_class_num += class_num * len(batch_att)
+
+    loss_rel_mean = total_loss_rel / total_class_num
+    acc_rel = total_correct_num_rel / sample_num
+    return loss_rel_mean, acc_rel
 
 def main():
     # step 1: init dataset
@@ -129,7 +160,6 @@ def main():
     class_embedding = 'original_att_splits'
 
     attribute_values = np.load("CUB_200_2011/attribute_values.npy")
-    certainty_values = np.load("CUB_200_2011/certainty_values.npy")
 
     matcontent = sio.loadmat(dataroot + "/" + dataset + "/" + image_embedding + ".mat")
     feature = matcontent['features'].T
@@ -147,7 +177,6 @@ def main():
     x = feature[trainval_loc] # train_features
 
     train_attributes = attribute_values[trainval_loc] # train_attribute_values
-    train_certainty = certainty_values[trainval_loc] # train_certainty_values
 
     train_label = label[trainval_loc].astype(int)  # train_label
 
@@ -168,9 +197,21 @@ def main():
 
     train_label = torch.from_numpy(train_label).unsqueeze(1) # [7057, 1]
 
-    train_attributes = torch.from_numpy(train_attributes) # train_attribute_values
+    train_attributes = torch.from_numpy(train_attributes) # [7057, 312]
+    
+    num_valid = int(len(train_label) * 0.2)
 
-    train_certainty = torch.from_numpy(train_certainty) # train_certainty_values
+    valid_features = train_features[-num_valid:]
+    
+    valid_label = train_label[-num_valid:]
+
+    valid_attributes = train_attributes[-num_valid:]
+
+    train_features = train_features[:-num_valid]
+    
+    train_label = train_label[:-num_valid]
+
+    train_attributes = train_attributes[:-num_valid]
 
     # attributes
     all_attributes = np.array(attribute) # (200, 312)
@@ -189,133 +230,120 @@ def main():
     test_seen_features = torch.from_numpy(x_test_seen) # [1764, 2048]
     
     test_seen_label = torch.from_numpy(test_label_seen) # [1764]
-    
-    #train_data = TensorDataset(train_features,train_label)
-    train_data = IntegratedDataset(train_features, train_label, train_attributes, train_certainty)    
 
     # init network
     print("init networks")
-    attribute_network = AttributeNetwork(2048,1200,624).cuda()
-    relation_network = RelationNetwork(936, 400).cuda()
+    attribute_network = AttributeNetwork(2048, 1200, 312).cuda()
+    relation_network = RelationNetwork(624, 300).cuda()
+    
+    #train_data = TensorDataset(train_features,train_label)
+    train_data = IntegratedDataset(train_features, train_label, train_attributes)
+    valid_data = IntegratedDataset(valid_features, valid_label, valid_attributes)
 
     mse = nn.MSELoss().cuda()
-    ce = nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 1.])).cuda()
+    #ce = nn.CrossEntropyLoss(weight=torch.FloatTensor([0.1, 1.])).cuda()
     nll = torch.nn.NLLLoss(weight=torch.FloatTensor([0.1, 1.])).cuda()
     #mse = nn.BCELoss().cuda()
 
     train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+    valid_loader = DataLoader(valid_data, batch_size=BATCH_SIZE, shuffle=False)
 
     if MODEL == 1:
         attribute_network_optim = torch.optim.Adam(attribute_network.parameters(), lr=LEARNING_RATE, weight_decay=1e-5)
         #attribute_network_optim = torch.optim.SGD(attribute_network.parameters(), lr=1e-3, momentum=0.9, weight_decay=1e-4, nesterov=True)
         attribute_network_scheduler = StepLR(attribute_network_optim, step_size=30000, gamma=0.5)
 
-        # if os.path.exists("./models/zsl_cub2_attribute_network_v35.pkl"):
-        #     attribute_network.load_state_dict(torch.load("./models/zsl_cub_attribute_network_v35.pkl"))
-        #     print("load attribute network success")
-        # if os.path.exists("./models/zsl_cub2_relation_network_v35.pkl"):
-        #     relation_network.load_state_dict(torch.load("./models/zsl_cub_relation_network_v35.pkl"))
-        #     print("load relation network success")
+        print("training attribute network...")
 
-        print("training...")
-        #last_accuracy = 0.0
         total_steps = 0
         best_loss_att = 10000
-        best_loss_cer = 10000
 
         for epoch in range(EPOCHS):
             attribute_network.train()
-            for i, (batch_features, batch_labels, batch_att, batch_cer) in enumerate(train_loader):
+            for i, (batch_features, batch_labels, batch_att) in enumerate(train_loader):
 
-                batch_features, batch_att, batch_cer = batch_features.float().cuda(), batch_att.float().cuda(), batch_cer.float().cuda()
+                batch_features, batch_att = batch_features.float().cuda(), batch_att.float().cuda()
                 
                 attribute_network_scheduler.step(total_steps)
                 
-                #relation_network_scheduler.step(episode)
-
-                #sample_labels = set(batch_labels.squeeze().numpy().tolist())
-                
-                #sample_attributes = torch.Tensor([all_attributes[i] for i in sample_labels])
-                
-                #class_num = sample_attributes.shape[0]
                 pred_embeddings = attribute_network(batch_features)
 
                 cat_batch_att = batch_att.long().view(-1)
-                cat_pred_att = torch.cat((pred_embeddings[:,0:312].unsqueeze(2), 1 - pred_embeddings[:,0:312].unsqueeze(2)), 2).view(-1, 2)
+                cat_pred_att = torch.cat((pred_embeddings.unsqueeze(2), 1 - pred_embeddings.unsqueeze(2)), 2).view(-1, 2)
                 
                 #loss_att = ce(cat_pred_att, cat_batch_att)
                 loss_att = nll(torch.log(cat_pred_att + 1e-10), cat_batch_att)
-                loss_cer = mse(pred_embeddings[:,312:624], batch_cer)
-                loss_net1 = loss_att + loss_cer
 
                 attribute_network_optim.zero_grad()
-                loss_net1.backward()
+                loss_att.backward()
                 attribute_network_optim.step()
 
                 total_steps += 1
 
             #if epoch % 50 == 0:
-            loss_att_mean, loss_cer_mean, acc_att, acc_cer = evaluate_attribute_network(attribute_network, nll, mse, train_loader)
-            print("Epoch: {:>3} loss_att: {:.5f} loss_cer: {:.5f}".format(epoch, loss_att_mean, loss_cer_mean))
-            print("Epoch: {:>3} acc_att: {:.5f} acc_cer: {:.5f}".format(epoch, acc_att, acc_cer))
+            loss_att_mean, acc_att = evaluate_attribute_network(attribute_network, nll, valid_loader)
+            print("Epoch: {:>3} loss_att: {:.5f}".format(epoch, loss_att_mean))
+            print("Epoch: {:>3} acc_att: {:.5f}".format(epoch, acc_att))
 
-            if best_loss_att > loss_att_mean and best_loss_cer > loss_cer_mean:
-                torch.save(attribute_network.state_dict(), 'models/attribute_network.pt')
             if best_loss_att > loss_att_mean:
+                torch.save(attribute_network.state_dict(), 'models/attribute_network.pt')
                 best_loss_att = loss_att_mean
-            if best_loss_cer > loss_cer_mean:
-                best_loss_cer = loss_cer_mean
 
     else:
-        attribute_network.load_state_dict(torch.load("./models/attribute_network.pt"))
-        print("load attribute network success")
-
-        loss_att_mean, loss_cer_mean = evaluate_attribute_network(attribute_network, ce, train_loader)
-        print("loss_att: {:.5f} loss_cer: {:.5f}".format(loss_att_mean, loss_cer_mean))
-
-        assert 1 == 0
-        relation_network_optim = torch.optim.Adam(relation_network.parameters(),lr=LEARNING_RATE)
+        relation_network_optim = torch.optim.Adam(relation_network.parameters(), lr=LEARNING_RATE, weight_decay=1e-5)
         relation_network_scheduler = StepLR(relation_network_optim, step_size=30000, gamma=0.5)
 
+        print("training relation network...")
 
+        total_steps = 0
+        best_loss_rel = 10000
 
-        
-        batch_features = Variable(batch_features).cuda(GPU).float()  # 32*2048
-        sample_features = attribute_network(Variable(sample_attributes).cuda(GPU)) #c*2048
+        for epoch in range(EPOCHS):
+            relation_network.train()
+            for i, (batch_features, batch_labels, batch_att) in enumerate(train_loader):
+                batch_att = batch_att.float().cuda()
 
-        sample_features_ext = sample_features.unsqueeze(0).repeat(BATCH_SIZE,1,1)
-        batch_features_ext = batch_features.unsqueeze(0).repeat(class_num,1,1)
-        batch_features_ext = torch.transpose(batch_features_ext,0,1)
-        
-        #print(sample_features_ext)
-        #print(batch_features_ext)
-        relation_pairs = torch.cat((sample_features_ext,batch_features_ext),2).view(-1,4096)
-        relations = relation_network(relation_pairs).view(-1,class_num)
-        #print(relations)
+                relation_network_scheduler.step(total_steps)
 
-        # re-build batch_labels according to sample_labels
-        sample_labels = np.array(sample_labels)
-        re_batch_labels = []
-        for label in batch_labels.numpy():
-            index = np.argwhere(sample_labels==label)
-            re_batch_labels.append(index[0][0])
-        re_batch_labels = torch.LongTensor(re_batch_labels)
+                sample_labels = set(batch_labels.squeeze().numpy().tolist())
+                        
+                sample_embeddings = torch.Tensor([all_attributes[i] for i in sample_labels]).cuda()
 
-        # loss
-        # relations = nn.functional.softmax(relations, 1)
-        mse = nn.MSELoss().cuda(GPU)
-        one_hot_labels = Variable(torch.zeros(BATCH_SIZE, class_num).scatter_(1, re_batch_labels.view(-1,1), 1)).cuda(GPU)
-        loss = mse(relations,one_hot_labels)
+                class_num = sample_embeddings.shape[0]
 
-        # update
-        attribute_network.zero_grad()
-        relation_network.zero_grad()
+                embeddings_bunch = sample_embeddings.unsqueeze(0).repeat(len(batch_att), 1, 1)
+                attributes_bunch = batch_att.unsqueeze(0).repeat(class_num, 1, 1)
+                attributes_bunch = torch.transpose(attributes_bunch, 0, 1)
+                
+                relation_pairs = torch.cat((embeddings_bunch, attributes_bunch), 2).view(-1, 624)
+                relations = relation_network(relation_pairs).view(-1, class_num)
 
-        loss.backward()
+                # re-build batch_labels according to sample_labels
+                sample_labels = np.array(list(sample_labels))
+                re_batch_labels = []
+                for label in batch_labels.numpy():
+                    index = np.argwhere(sample_labels == label)
+                    re_batch_labels.append(index[0][0])
+                re_batch_labels = torch.LongTensor(re_batch_labels)
+                one_hot_labels = torch.zeros(len(batch_att), class_num).scatter_(1, re_batch_labels.view(-1, 1), 1).cuda()
+                
+                loss_rel = mse(relations, one_hot_labels)
 
-        attribute_network_optim.step()
-        relation_network_optim.step()
+                # update
+                relation_network.zero_grad()
+                loss_rel.backward()
+                relation_network_optim.step()
 
+            #if epoch % 50 == 0:
+            loss_rel_mean, acc_rel = evaluate_relation_network(relation_network, mse, valid_loader, all_attributes)
+            print("Epoch: {:>3} loss_rel: {:.5f}".format(epoch, loss_rel_mean))
+            print("Epoch: {:>3} acc_rel: {:.5f}".format(epoch, acc_rel))
+
+            if best_loss_rel > loss_rel_mean:
+                torch.save(relation_network.state_dict(), 'models/relation_network.pt')
+                best_loss_rel = loss_rel_mean
+
+        assert 1 == 0
         # if (episode+1)%100 == 0:
         #         print("episode:",episode+1,"loss",loss.data[0])
 
